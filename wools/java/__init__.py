b514fcfb68455aa995019ac1fe4b8f3ee1eb958c
Stefan Zimmermann <szimmermann@advaoptical.com>
2017-07-17 15:25:38 +0200

    Adapt changes of alpakka.Wool design

--- wools/java/__init__.py
+++ wools/java/__init__.py
@@ -3,8 +3,6 @@ from alpakka import register_wool
 
 WOOL = register_wool('Java', __name__)
 
-PARENT_WRAPPERS = WOOL.PARENT_YANG_NODE_WRAPPERS
 
-
-class JavaGrouping(PARENT_WRAPPERS['grouping']):
+class JavaGrouping(WOOL.parent['grouping']):
     pass
--- wools/java/akka/__init__.py
+++ wools/java/akka/__init__.py
@@ -3,8 +3,6 @@ from alpakka import register_wool
 
 WOOL = register_wool('Akka', __name__, parent='Java')
 
-PARENT_WRAPPERS = WOOL.PARENT_YANG_NODE_WRAPPERS
 
-
-class AkkaGrouping(PARENT_WRAPPERS['grouping']):
+class AkkaGrouping(WOOL.parent['grouping']):
     pass
